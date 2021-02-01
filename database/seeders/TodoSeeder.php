d0213786006b6656ea156caf68abb8ce7be2a854
Bruno Garcia <mail.brunogarcia@gmail.com>
2021-02-01 17:10:05 -0300

    .

--- database/seeders/TodoSeeder.php
+++ database/seeders/TodoSeeder.php
@@ -16,16 +16,6 @@ class TodoSeeder extends Seeder
      */
     public function run()
     {
-        /*User::all()->each(function($user){
-            $user->todos()->saveMany(
-                factory(Todo::class, 10)->make()
-            )->each(function($todo){
-                $todo->tasks()->saveMany(
-                    factory(TodoTask::class)->make()
-                );
-            });
-        });
-        */
         User::all()->each(function($user){
             $user->todos()->saveMany(
                 Todo::factory()->count(10)->make()
